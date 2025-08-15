e46aeac9a33ff6032cf909f96520e885b9db2b73
Zahra <zahrabayat367@gmail.com>
2025-08-15 14:17:28 +0330

    refactor: :recycle: Clean up QuizList & QuizCard comonents

--- src/components/Quiz/QuizList.tsx
+++ src/components/Quiz/QuizList.tsx
@@ -5,21 +5,27 @@ import QuizCard from "./QuizCard";
 
 const QuizList = () => {
   const { data: questions = [] } = useGetQuestions();
-  return questions.map((question: QuestionModel) => (
-    <div className="flex flex-col rounded-xl border border-foreground-tertiary p-6">
-      <section className="flex items-center justify-between">
-        <p className="text-foreground-tertiary">John Doe Feb 28 , 2025</p>
-        <div className="flex gap-3">
-          <button className="flex items-center rounded-2xl bg-white px-4 py-1 text-my-secondary">
-            Publish
-          </button>
-          <img className="object-contain" src={Vector} alt="Vector" />
+  return (
+    <div className="flex flex-col gap-10">
+      {questions.map((question: QuestionModel) => (
+        <div className="flex flex-col gap-10">
+          <div className="rounded-xl border border-foreground-tertiary p-6">
+            <section className="flex items-center justify-between">
+              <p className="text-foreground-tertiary">John Doe Feb 28 , 2025</p>
+              <div className="flex gap-3">
+                <button className="flex items-center rounded-2xl bg-white px-4 py-1 text-my-secondary">
+                  Publish
+                </button>
+                <img className="object-contain" src={Vector} alt="Vector" />
+              </div>
+            </section>
+            <hr className="border-0.5 mt-5 mb-3 border-foreground-tertiary"></hr>
+            <QuizCard key={question.id} question={question} />
+          </div>
         </div>
-      </section>
-      <hr className="border-0.5 mt-5 mb-3 border-foreground-tertiary"></hr>
-      <QuizCard key={question.id} question={question} />
+      ))}
     </div>
-  ));
+  );
 };
 
 export default QuizList;

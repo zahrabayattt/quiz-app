9bdaa4df7f58a788ec505a051852580dbc745520
Zahra <zahrabayat367@gmail.com>
2025-08-15 11:36:34 +0330

    feat: :sparkles: Navigate from dashboard to create quiz page

--- src/pages/QuizDashboard.tsx
+++ src/pages/QuizDashboard.tsx
@@ -1,16 +1,22 @@
+import { useNavigate } from "react-router";
 import pluscircle from "../assets/images/plus-circle.png";
 import Vector from "../assets/images/Vector.png";
 import LayoutContainer from "../components/Layout/LayoutContainer";
 import Navbar from "../components/Layout/Navbar";
 import QuizList from "../components/Quiz/QuizList";
+
 const QuizDashboard = () => {
+  const navigate = useNavigate();
   return (
     <div className="min-h-screen bg-background">
       <Navbar />
       <LayoutContainer>
         <section className="my-5 flex items-center justify-between">
           <h2 className="text-2xl font-bold text-foreground">Quiz Dashboard</h2>
-          <button className="my-btn flex items-center gap-2 bg-my-primary text-foreground">
+          <button
+            className="my-btn flex cursor-pointer items-center gap-2 bg-my-primary text-foreground"
+            onClick={() => navigate("/create-quiz")}
+          >
             <img className="object-contain" src={pluscircle} alt="pluscircle" />
             New Quiz
           </button>
